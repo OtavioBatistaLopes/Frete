7089235429f0f946ecdf43c40c02022f65694918
otavi <otavi@DESKTOP-SQ3CC63>
2022-11-08 15:41:05 -0300

    Adicionar arquivos de projeto.

---
+++ FreteDominato/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace FreteDominato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtAutonomia = new System.Windows.Forms.TextBox();
+            this.txtLocalidade = new System.Windows.Forms.TextBox();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.boxTabela = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtCustoPL = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imgVeiculo = new System.Windows.Forms.PictureBox();
+            this.txtkms = new System.Windows.Forms.TextBox();
+            this.txtCusto = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtDespesa = new System.Windows.Forms.TextBox();
+            this.txtConsumo = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgVeiculo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAutonomia
+            // 
+            resources.ApplyResources(this.txtAutonomia, "txtAutonomia");
+            this.txtAutonomia.Name = "txtAutonomia";
+            // 
+            // txtLocalidade
+            // 
+            resources.ApplyResources(this.txtLocalidade, "txtLocalidade");
+            this.txtLocalidade.Name = "txtLocalidade";
+            // 
+            // txtDistancia
+            // 
+            resources.ApplyResources(this.txtDistancia, "txtDistancia");
+            this.txtDistancia.Name = "txtDistancia";
+            // 
+            // boxTabela
+            // 
+            this.boxTabela.FormattingEnabled = true;
+            this.boxTabela.Items.AddRange(new object[] {
+            resources.GetString("boxTabela.Items"),
+            resources.GetString("boxTabela.Items1")});
+            resources.ApplyResources(this.boxTabela, "boxTabela");
+            this.boxTabela.Name = "boxTabela";
+            this.boxTabela.SelectedIndexChanged += new System.EventHandler(this.boxTabela_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.txtCustoPL);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.txtAutonomia);
+            this.panel1.Controls.Add(this.boxTabela);
+            this.panel1.Controls.Add(this.txtLocalidade);
+            this.panel1.Controls.Add(this.txtDistancia);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // txtCustoPL
+            // 
+            resources.ApplyResources(this.txtCustoPL, "txtCustoPL");
+            this.txtCustoPL.Name = "txtCustoPL";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FreteDominato.Properties.Resources.download;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imgVeiculo
+            // 
+            resources.ApplyResources(this.imgVeiculo, "imgVeiculo");
+            this.imgVeiculo.Name = "imgVeiculo";
+            this.imgVeiculo.TabStop = false;
+            // 
+            // txtkms
+            // 
+            resources.ApplyResources(this.txtkms, "txtkms");
+            this.txtkms.Name = "txtkms";
+            // 
+            // txtCusto
+            // 
+            resources.ApplyResources(this.txtCusto, "txtCusto");
+            this.txtCusto.Name = "txtCusto";
+            // 
+            // txtTotal
+            // 
+            resources.ApplyResources(this.txtTotal, "txtTotal");
+            this.txtTotal.Name = "txtTotal";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // txtDespesa
+            // 
+            resources.ApplyResources(this.txtDespesa, "txtDespesa");
+            this.txtDespesa.Name = "txtDespesa";
+            // 
+            // txtConsumo
+            // 
+            resources.ApplyResources(this.txtConsumo, "txtConsumo");
+            this.txtConsumo.Name = "txtConsumo";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5"),
+            resources.GetString("comboBox1.Items6"),
+            resources.GetString("comboBox1.Items7"),
+            resources.GetString("comboBox1.Items8"),
+            resources.GetString("comboBox1.Items9"),
+            resources.GetString("comboBox1.Items10"),
+            resources.GetString("comboBox1.Items11"),
+            resources.GetString("comboBox1.Items12"),
+            resources.GetString("comboBox1.Items13"),
+            resources.GetString("comboBox1.Items14"),
+            resources.GetString("comboBox1.Items15"),
+            resources.GetString("comboBox1.Items16"),
+            resources.GetString("comboBox1.Items17"),
+            resources.GetString("comboBox1.Items18"),
+            resources.GetString("comboBox1.Items19"),
+            resources.GetString("comboBox1.Items20"),
+            resources.GetString("comboBox1.Items21"),
+            resources.GetString("comboBox1.Items22"),
+            resources.GetString("comboBox1.Items23"),
+            resources.GetString("comboBox1.Items24"),
+            resources.GetString("comboBox1.Items25"),
+            resources.GetString("comboBox1.Items26")});
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtConsumo);
+            this.Controls.Add(this.txtDespesa);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtCusto);
+            this.Controls.Add(this.txtkms);
+            this.Controls.Add(this.imgVeiculo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgVeiculo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtAutonomia;
+        private System.Windows.Forms.TextBox txtLocalidade;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.ComboBox boxTabela;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox imgVeiculo;
+        private System.Windows.Forms.TextBox txtkms;
+        private System.Windows.Forms.TextBox txtCusto;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtDespesa;
+        private System.Windows.Forms.TextBox txtConsumo;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtCustoPL;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ FreteDominato/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FreteDominato
+{
+    public partial class Form1 : Form
+    {
+        float kmRodado = 0;
+        float kmAutonimia = 0;
+        float QuantidadeCombustivel = 0;
+        double valorCombustivel = 0;
+        double custoCombustivel;
+        double valorTotal;
+        double custoKm;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void boxTabela_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (boxTabela.Text == "Carro")
+            {
+                imgVeiculo.Image = FreteDominato.Properties.Resources.Saveiro;
+            }
+            else
+            {
+                imgVeiculo.Image = Properties.Resources.caminhão;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            kmRodado = float.Parse(txtDistancia.Text);
+
+            float kmDobrado = 0;
+
+            if (checkBox1.Checked == false)
+            {
+                kmDobrado = kmRodado * 1; // ida
+            }
+            else
+            {
+                kmDobrado = kmRodado * 2; // volta
+            }
+            //consumo combustivel
+            QuantidadeCombustivel =kmDobrado / float.Parse(txtAutonomia.Text);
+            txtConsumo.Text = QuantidadeCombustivel.ToString("N0") + " Litros";
+
+            //custo combustivel
+            custoCombustivel = double.Parse(txtCustoPL.Text) * QuantidadeCombustivel;
+
+            txtDespesa.Text = custoCombustivel.ToString("N2");
+
+            //Valor Frete
+            valorTotal = custoCombustivel * 3;
+            txtTotal.Text = valorTotal.ToString("N2");
+
+            //KM Rodados
+            txtkms.Text = kmDobrado.ToString();
+
+            //Custo por km
+            custoKm = double.Parse(txtCustoPL.Text) / double.Parse(txtAutonomia.Text);
+            txtCusto.Text = custoKm.ToString("N2");
+
+        }
+
+        
+    }
+}
+
+
+
+
+
